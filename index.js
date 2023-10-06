91fe2522b73657316f39736dfb05e9927111edad
Nitin Mishra <mnitin2311@gmail.com>
2023-10-05 21:39:47 -0400

    Modified: Completed with updating data

--- index.js
+++ index.js
@@ -58,19 +58,47 @@ app.post("/g/license", async (req, res) => {
     const user = await UserModel.findOne({ LicenseNo: licenseNo });
 
     if (!user) {
-      return res.status(404).send("User not found");
+      return res.status(404).render("userNotFound");
     }
 
-    console.log("Found USER DATA", user);
-
-    // Assuming 'gtest' is a template that you are rendering
     res.render("userInfo", { user });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
 });
-
+app.post("/edit", async (req, res) => {
+  const data = req.body;
+  const license = data.licenseNo;
+  const UserData = await UserModel.findOne({
+    LicenseNo: license,
+  });
+  // console.log(UserData);
+  try {
+    const updateData = await UserModel.updateOne(
+      { LicenseNo: license },
+      {
+        $set: {
+          "carDetails.make": data.make,
+          "carDetails.model": data.model,
+          "carDetails.year": data.year,
+          "carDetails.plateno": data.plateno,
+        },
+      }
+    );
+    // console.log(updateData);
+    if (!updateData.acknowledged) {
+      console.log("Data not updated");
+      return res.status(404).render("gtest");
+    } else {
+      res.render("gtest");
+    }
+  } catch (error) {
+    console.error("Error updating data:", error);
+    // Handle the error appropriately (e.g., send an error response)
+    res.status(500).send("Internal Server Error");
+  }
+});
 // Route to Login Page
 app.get("/login", (req, res) => {
   res.render("login");
