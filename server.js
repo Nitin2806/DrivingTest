740054289ae6e6cc27839b22379bfb6b1132711a
Nitin Mishra <mnitin2311@gmail.com>
2023-11-19 23:27:51 -0500

    added:removed cookies policies

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use(
     proxy: true,
     name: "thisshouldwork",
     cookie: {
-      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      httpOnly: false,
-      sameSite: "none",
     },
   })
 );
