128093012d3729f653b0bd0321b726d912637a18
Nitin Mishra <mnitin2311@gmail.com>
2023-11-19 23:00:13 -0500

    added: return statements

--- controllers/loginUserAccount.js
+++ controllers/loginUserAccount.js
@@ -4,9 +4,9 @@ const createAccountModel = require("../models/UserAccount");
 module.exports = async (req, res) => {
   const { userName, password } = req.body;
   if (userName == "") {
-    res.render("login", { error: "Please enter your user id or email" });
+    return res.render("login", { error: "Please enter your user id or email" });
   } else if (password == "") {
-    res.render("login", { error: "Please enter your Password" });
+    return res.render("login", { error: "Please enter your Password" });
   } else {
     let findUser = await createAccountModel.findOne({ userName: userName });
 
@@ -19,10 +19,10 @@ module.exports = async (req, res) => {
         if (same) {
           req.session.userId = findUser._id;
           req.session.userType = findUser.userType;
-          res.redirect("/");
+          return res.redirect("/");
         } else {
           console.error("Wrong Password");
-          res.render("login", { error: "Wrong Password" });
+          return res.render("login", { error: "Wrong Password" });
         }
       });
     }
